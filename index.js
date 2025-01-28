43c3c5553ea0a67afac3efee9ec7ee1e9fe2c8db
kamesh singh <88535029+kamesh7773@users.noreply.github.com>
2025-01-28 20:05:06 +0530

    commit from home

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require('express');
 const admin = require('firebase-admin');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4500;
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert({
-    "type": "service_account",
-    "project_id": process.env.project_id,
-    "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
-    "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": process.env.client_x509_cert_url,
-    "universe_domain": "googleapis.com"
+    type: 'service_account',
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key.replace(/\\n/g, '\n'), // Fix for private key formatting
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+    token_uri: 'https://oauth2.googleapis.com/token',
+    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+    client_x509_cert_url: process.env.client_x509_cert_url,
+    universe_domain: 'googleapis.com',
   }),
 });
 
@@ -26,11 +26,16 @@ app.use(express.json());
 
 // Endpoint to send notifications
 app.post('/send-notification', async (req, res) => {
-  const { recipientToken, message } = req.body;
+  const { recipientToken , title, message } = req.body;
+
+  // Validate request body
+  if (!recipientToken || !message) {
+    return res.status(400).send('Missing required fields: recipientToken or message');
+  }
 
   const payload = {
     notification: {
-      title: 'New Message',
+      title: title, 
       body: message,
     },
     token: recipientToken,
@@ -38,12 +43,16 @@ app.post('/send-notification', async (req, res) => {
 
   try {
     await admin.messaging().send(payload);
-    res.status(200).send('Notification sent successfully');
+    res.status(200).send('Notification sent successfully ✅');
   } catch (error) {
     console.error('Error sending notification:', error);
-    res.status(500).send('Failed to send notification');
+    res.status(500).send(`Failed to send notification ⛔: ${error.message}`);
   }
 });
+
+app.get("/", (req, res) =>
+  res.send("Email OTP server running successfully ✅")
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
