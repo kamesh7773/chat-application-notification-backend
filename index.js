5007dab1afa26e0d65ade869e25e78db504331ee
kamesh singh <88535029+kamesh7773@users.noreply.github.com>
2025-01-29 10:07:06 +0530

    commit from office

--- index.js
+++ index.js
@@ -2,24 +2,12 @@ const express = require("express");
 const admin = require("firebase-admin");
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4500;
 
 // Initialize Firebase Admin SDK
 const serviceAccount = require("./config/serviceAccountKey.json"); // Update the path here
 admin.initializeApp({
-  credential: admin.credential.cert({
-    "type": "service_account",
-    "project_id": process.env.project_id,
-    "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
-    "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": process.env.client_x509_cert_url,
-    "universe_domain": "googleapis.com"
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 // Middleware to parse JSON
