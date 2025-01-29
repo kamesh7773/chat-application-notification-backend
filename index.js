d5cc9704866d17171dd0dd22885bacd559944b9b
kamesh singh <88535029+kamesh7773@users.noreply.github.com>
2025-01-29 10:04:50 +0530

    commit from office

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-const express = require('express');
-const admin = require('firebase-admin');
+const express = require("express");
+const admin = require("firebase-admin");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 // Initialize Firebase Admin SDK
+const serviceAccount = require("./config/serviceAccountKey.json"); // Update the path here
 admin.initializeApp({
-  credential: admin.credential.cert({
-    "type": "service_account",
-    "project_id": process.env.project_id,
-    "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key,
-    "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": process.env.client_x509_cert_url,
-    "universe_domain": "googleapis.com"
-  }),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Endpoint to send notifications
-app.post('/send-notification', async (req, res) => {
+app.post("/send-notification", async (req, res) => {
   const { recipientToken, message } = req.body;
 
   const payload = {
     notification: {
-      title: 'New Message',
+      title: "New Message",
       body: message,
     },
     token: recipientToken,
@@ -38,10 +27,10 @@ app.post('/send-notification', async (req, res) => {
 
   try {
     await admin.messaging().send(payload);
-    res.status(200).send('Notification sent successfully');
+    res.status(200).send("Notification sent successfully");
   } catch (error) {
-    console.error('Error sending notification:', error);
-    res.status(500).send('Failed to send notification');
+    console.error("Error sending notification:", error);
+    res.status(500).send("Failed to send notification");
   }
 });
 
